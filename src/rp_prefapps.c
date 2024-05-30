d12148c5b15a4280be47d28f30917273a54dceed
Simon Long <simon@raspberrypi.com>
2024-05-30 14:54:37 +0100

    Fix warning

--- src/rp_prefapps.c
+++ src/rp_prefapps.c
@@ -1377,7 +1377,7 @@ static gboolean quit (GtkButton *button, gpointer data)
         err_dlg = NULL;
     }
 
-    if ((int) data == 1) system ("reboot");
+    if ((long) data == 1) system ("reboot");
 
     gtk_main_quit ();
     return FALSE;
