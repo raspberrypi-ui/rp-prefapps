9406c820813ab887165a8f844bddb76cd0cbc308
Simon Long <simon@raspberrypi.com>
2025-03-27 12:28:45 +0000

    Enmeson

--- src/rp_prefapps.c
+++ src/rp_prefapps.c
@@ -25,15 +25,12 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
 #define _GNU_SOURCE
 #include <string.h>
 #include <math.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <locale.h>
 
 #include <glib.h>
 #include <glib/gi18n.h>
@@ -1572,12 +1569,10 @@ int main (int argc, char *argv[])
     GtkBuilder *builder;
     GtkCellRenderer *crp, *crt, *crb;
 
-#ifdef ENABLE_NLS
     setlocale (LC_ALL, "");
     bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
     bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
     textdomain (GETTEXT_PACKAGE);
-#endif
 
     if (system ("raspi-config nonint is_pi")) is_pi = FALSE;
     if (getenv ("WAYLAND_DISPLAY")) wayland = TRUE;
